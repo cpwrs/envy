4cfe9fc1724fab78a2e908fd128703611c71485c
Carson Powers <me@carsonp.net>
2024-02-20 21:55:30 -0600

    initial neovim derivation

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+	description = "Noevim flake";
+	inputs = {
+  	nixpkgs.url = "github:NixOS/nixpkgs";
+    neovim = {
+			url = "github:neovim/neovim?dir=contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+		};	
+  };
+  outputs = { self, nixpkgs, neovim }: {
+		packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
+		apps.x86_64-linux.default = {
+			type = "app";
+			program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
+		};
+	};
+}
