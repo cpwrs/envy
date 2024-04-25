f055fbcc56b650a30677cc0cd2641b03b2d0d756
Carson Powers <me@carsonp.net>
2024-04-25 17:39:35 -0500

    lower file count

--- envy.nix
+++ envy.nix
@@ -1,11 +1,37 @@
 { pkgs }:
 let
-	plugins = import ./plugins.nix { inherit pkgs; };
-	customRC = import ./conf.nix { inherit pkgs; };
-in 
+  # List all plugins we want ported to pkgs.vimPlugins
+	plugins = with pkgs.vimPlugins; [
+    oil-nvim
+	  telescope-nvim
+	  nvim-lspconfig
+	  trouble-nvim
+    nvim-treesitter.withAllGrammars
+  ];
+
+  # Make a derivation with our config files.
+  # Add all derivation files to our nvim runtimepath and source them.
+  sourceConfig = dir:
+    let
+      # Derivation where each file of conf/ is copied to.
+      configDir = pkgs.stdenv.mkDerivation {
+        name = "nvim-lua-config";
+        src = ./${dir};
+        installPhase = ''
+          mkdir -p $out/
+          cp -r ./* $out/
+        '';
+      };
+    in
+      # Return a string that sources the config entry point.
+      ''
+        set runtimepath+=${configDir}
+        luafile ${configDir}/init.lua
+      '';
+in
   pkgs.wrapNeovim pkgs.neovim {
     configure = {
-			inherit customRC;
+      customRC = sourceConfig "conf";
       packages.all.start = plugins;
     };
   }
--- plugins.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }:
-with pkgs.vimPlugins; [
-  oil-nvim
-	telescope-nvim
-	nvim-lspconfig
-	trouble-nvim
-  nvim-treesitter.withAllGrammars
-]
