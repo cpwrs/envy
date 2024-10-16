fc90a61323bc200963c33571317e9bac3d9e7428
Carson Powers <me@carsonp.net>
2024-10-16 13:25:16 -0500

    better windows

--- conf/init.lua
+++ conf/init.lua
@@ -11,3 +11,15 @@ require('highlights')
 require('statusline')
 require('plugs')
 require('lsp')
+
+-- Keymaps not specific to plugins
+
+-- Navigate panes with Ctrl+HJKL
+vim.keymap.set('n', '<C-h>', function() vim.cmd [[wincmd h]] end)
+vim.keymap.set('n', '<C-j>', function() vim.cmd [[wincmd j]] end)
+vim.keymap.set('n', '<C-k>', function() vim.cmd [[wincmd k]] end)
+vim.keymap.set('n', '<C-l>', function() vim.cmd [[wincmd l]] end)
+
+-- Start/end of line with H and L
+vim.keymap.set('n', 'H', '^')
+vim.keymap.set('n', 'L', '$')
