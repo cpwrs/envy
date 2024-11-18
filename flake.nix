64dbcb3d7ee11dff4e8731467b8798077919470c
Carson Powers <me@carsonp.net>
2024-11-17 20:46:30 -0600

    flake is back

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Tiny & fast neovim configuration";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    neovim.url = "github:nix-community/neovim-nightly-overlay";
+  };
+  outputs = { self, nixpkgs, neovim, flake-utils }:
+    let
+      overlayFlakeInputs = prev: final: {
+        neovim = neovim.packages.x86_64-linux.neovim;
+      };
+      overlayEnvy = prev: final: {
+        envy = let
+	        plugins = with final.vimPlugins; [
+            oil-nvim
+	          telescope-nvim
+	          nvim-lspconfig
+            nvim-treesitter.withAllGrammars
+          ];
+        in
+          final.wrapNeovim final.neovim {
+            configure = {
+              customRC = ''
+                set runtimepath+=~/.config/nvim
+                luafile ~/.config/nvim/init.lua
+              ''
+              packages.all.start = plugins;
+            };
+          }
+      };
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ overlayFlakeInputs overlayEnvy ];
+      };
+    in {
+      packages.x86_64-linux.default = pkgs.envy;
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${pkgs.envy}/bin/nvim";
+      };
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [ pkgs.lua-language-server ];
+      };
+    };
+}
