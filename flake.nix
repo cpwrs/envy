732907b8ad5b5919e0b2897ca77dfdff6d0c4836
Carson Powers <me@carsonp.net>
2024-12-22 17:09:10 -0600

    feat(flake): generic system support, refactor

--- flake.nix
+++ flake.nix
@@ -2,63 +2,69 @@
   description = "Tiny & fast neovim configuration";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     neovim.url = "github:nix-community/neovim-nightly-overlay";
+    blink.url = "github:Saghen/blink.cmp";
     americano = {
       flake = false;
       url = "github:cpwrs/americano.nvim";
     };
   };
-  outputs = { self, nixpkgs, neovim, americano }:
-    let
-      overlayFlakeInputs = prev: final: {
-        neovim = neovim.packages.x86_64-linux.neovim;
-      };
-      overlayEnvy = prev: final: {
-        envy = let
-          americano-plug = final.vimUtils.buildVimPlugin {
-            name = "americano";
-            src = americano;
-          };
-          plugins = with final.vimPlugins; [
-            oil-nvim
-            blink-cmp
-            friendly-snippets
-            americano-plug
-            telescope-nvim
-            nvim-lspconfig
-            nvim-treesitter.withAllGrammars
+  
+  outputs = { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      
+      perSystem = { config, self', pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              neovim = inputs.neovim.packages.${system}.neovim;
+              envy = let
+                plugins = with final.vimPlugins; [
+                  oil-nvim
+                  (final.vimUtils.buildVimPlugin {
+                    name = "americano";
+                    src = inputs.americano;
+                  })
+                  telescope-nvim
+                  nvim-lspconfig
+                  friendly-snippets
+                  nvim-treesitter.withAllGrammars
+                  inputs.blink.packages.${system}.default
+                ];
+                config = final.stdenv.mkDerivation {
+                  name = "nvim-config";
+                  src = ./.;
+                  installPhase = ''
+                    mkdir -p $out
+                    cp -r . $out/
+                  '';
+                };
+              in final.wrapNeovim final.neovim {
+                configure = {
+                  customRC = ''
+                    set runtimepath+=${config}
+                    luafile ${config}/init.lua
+                  '';
+                  packages.all.start = plugins;
+                };
+              };
+            })
           ];
-          config = pkgs.stdenv.mkDerivation {
-            name = "nvim-config";
-            src = ./.;
-            installPhase = ''
-              mkdir -p $out
-              cp -r . $out/
-            '';
-          };
-        in
-          final.wrapNeovim final.neovim {
-            configure = {
-              customRC = ''
-                set runtimepath+=${config}
-                luafile ${config}/init.lua
-              '';
-              packages.all.start = plugins;
-            };
-          };
-      };
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ overlayFlakeInputs overlayEnvy ];
-      };
-    in {
-      packages.x86_64-linux.default = pkgs.envy;
-      apps.x86_64-linux.default = {
-        type = "app";
-        program = "${pkgs.envy}/bin/nvim";
-      };
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = [ pkgs.lua-language-server ];
+        };
+
+        packages.default = pkgs.envy;
+        
+        apps.default = {
+          type = "app";
+          program = "${pkgs.envy}/bin/nvim";
+        };
+        
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.lua-language-server ];
+        };
       };
     };
 }
