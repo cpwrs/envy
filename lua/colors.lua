737ceb31538064524b668a97d79b9eeba7142821
Carson Powers <me@carsonp.net>
2025-02-07 00:55:43 -0600

    chore(nix): update americano

--- lua/colors.lua
+++ lua/colors.lua
@@ -5,13 +5,13 @@ americano.setup({
   terminal = true,
   overrides = {
     -- Add status line highlights for my custom line
-    StatusError = { fg = colors.red, bg = colors.dark },
-    StatusWarn = { fg = colors.orange, bg = colors.dark },
-    StatusInfo = { fg = colors.blue, bg = colors.dark },
-    StatusHint = { fg = colors.green, bg = colors.dark },
-    StatusLine = { fg = colors.white, bg = colors.dark },
-    StatusOther = { fg = colors.grey2, bg = colors.dark },
-    StatusImportant = { fg = colors.bright, bg = colors.dark, bold = true },
+    StatusError = { fg = colors.red, bg = colors.grey4 },
+    StatusWarn = { fg = colors.orange, bg = colors.grey4 },
+    StatusInfo = { fg = colors.blue, bg = colors.grey4 },
+    StatusHint = { fg = colors.green, bg = colors.grey4 },
+    StatusLine = { fg = colors.white, bg = colors.grey4 },
+    StatusOther = { fg = colors.grey1, bg = colors.grey4 },
+    StatusImportant = { fg = colors.bright, bg = colors.grey4, bold = true },
   }
 })
 americano.colorscheme()
