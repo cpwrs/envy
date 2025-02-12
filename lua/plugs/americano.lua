a4e22fbf995346adf65263188e4e79a415b18154
Carson Powers <me@carsonp.net>
2025-02-12 02:08:01 -0600

    feat: soft colors

--- lua/plugs/americano.lua
+++ lua/plugs/americano.lua
@@ -3,15 +3,16 @@ local colors = require('americano.colors')
 
 americano.setup({
   terminal = true,
+  soft = true,
   overrides = {
     -- Add status line highlights for my custom line
-    StatusError = { fg = colors.red, bg = colors.grey4 },
-    StatusWarn = { fg = colors.orange, bg = colors.grey4 },
-    StatusInfo = { fg = colors.blue, bg = colors.grey4 },
-    StatusHint = { fg = colors.green, bg = colors.grey4 },
-    StatusLine = { fg = colors.white, bg = colors.grey4 },
-    StatusOther = { fg = colors.grey1, bg = colors.grey4 },
-    StatusImportant = { fg = colors.bright, bg = colors.grey4, bold = true },
+    StatusError = { fg = colors.red, bg = colors.soft.grey4 },
+    StatusWarn = { fg = colors.orange, bg = colors.soft.grey4 },
+    StatusInfo = { fg = colors.blue, bg = colors.soft.grey4 },
+    StatusHint = { fg = colors.green, bg = colors.soft.grey4 },
+    StatusLine = { fg = colors.white, bg = colors.soft.grey4 },
+    StatusOther = { fg = colors.soft.grey1, bg = colors.soft.grey4 },
+    StatusImportant = { fg = colors.bright, bg = colors.soft.grey4, bold = true },
   }
 })
 americano.colorscheme()
