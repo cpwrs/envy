1d09896a78f7a4c6346a9fad5132f2256b41c9e4
Carson Powers <me@carsonp.net>
2025-05-02 21:55:45 -0500

    style: change statusline

--- lua/plugs/americano.lua
+++ lua/plugs/americano.lua
@@ -6,13 +6,13 @@ americano.setup({
   terminal = true,
   overrides = {
     -- Add status line highlights for my custom line
-    StatusError = { fg = p.red, bg = p.grey4 },
-    StatusWarn = { fg = p.orange, bg = p.grey4 },
-    StatusInfo = { fg = p.blue, bg = p.grey4 },
-    StatusHint = { fg = p.green, bg = p.grey4 },
-    StatusLine = { fg = p.white, bg = p.grey4 },
-    StatusOther = { fg = p.grey1, bg = p.grey4 },
-    StatusImportant = { fg = p.cream, bg = p.grey4, bold = true },
+    StatusError = { fg = p.red, bg = p.bg },
+    StatusWarn = { fg = p.orange, bg = p.bg },
+    StatusInfo = { fg = p.blue, bg = p.bg },
+    StatusHint = { fg = p.green, bg = p.bg },
+    StatusLine = { fg = p.white, bg = p.bg },
+    StatusOther = { fg = p.grey1, bg = p.bg },
+    StatusImportant = { fg = p.cream, bg = p.bg, bold = true },
   }
 })
 
